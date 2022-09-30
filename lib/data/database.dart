ebcb1d0b9566646c2ead8ac6c74b56cd2834f845
halodewakoding <halodewakoding@gmail.com>
2022-09-30 08:21:23 +0700

    yotube video

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -5,27 +5,18 @@ import 'package:drift/native.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 
-// assuming that your file is called filename.dart. This will give an error at
-// first, but it's needed for drift to know about the generated code
 part 'database.g.dart';
 
-// this will generate a table called "todos" for us. The rows of that table will
-// be represented by a class called "Todo".
 class Todos extends Table {
   IntColumn get id => integer().autoIncrement()();
   TextColumn get title => text().withLength(min: 6, max: 32)();
   TextColumn get detail => text()();
 }
 
-// this annotation tells drift to prepare a database class that uses both of the
-// tables we just defined. We'll see how to use that database class in a moment.
 @DriftDatabase(tables: [Todos])
 class MyDatabase extends _$MyDatabase {
-  // we tell the database where to store the data with this constructor
   MyDatabase() : super(_openConnection());
 
-  // you should bump this number whenever you change or add a table definition.
-  // Migrations are covered later in the documentation.
   @override
   int get schemaVersion => 1;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:keuangan_drift/data/database.dart';
+import 'package:todolist_youtube/data/database.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Aplikasi Duit'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final database = MyDatabase();
   TextEditingController titleTEC = new TextEditingController();
   TextEditingController detailTEC = new TextEditingController();
-  final database = MyDatabase();
 
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  Future<void> addTodo(String title, String detail) async {
-    final database = MyDatabase();
-
-    // Simple insert:
+  Future insert(String title, String detail) async {
     await database
         .into(database.todos)
         .insert(TodosCompanion.insert(title: title, detail: detail));
   }
 
-  Future<List<Todo>>? getAllTodo() {
-    final database = MyDatabase();
+  Future<List<Todo>> getAll() {
     return database.select(database.todos).get();
   }
 
-  Future updateTodo(Todo todo, title, detail) async {
-    try {
-      await database
-          .update(database.todos)
-          .replace(Todo(id: todo.id, title: title, detail: detail));
-    } catch (err) {
-      print("AUFAR " + err.toString());
-    }
+  Future update(Todo todo, String newTitle, String newDetail) async {
+    await database
+        .update(database.todos)
+        .replace(Todo(id: todo.id, title: newTitle, detail: newDetail));
   }
 
-  Future deleteTodo(Todo todo) async {
+  Future delete(Todo todo) async {
     await database.delete(database.todos).delete(todo);
   }
 
-  void addOrDetailransaksi(Todo? todo) {
+  void todoDialog(Todo? todo) {
     if (todo != null) {
-      titleTEC.text = todo.title.toString();
-      detailTEC.text = todo.detail.toString();
+      titleTEC.text = todo.title;
+      detailTEC.text = todo.detail;
     }
     showDialog(
         context: context,
@@ -80,63 +59,61 @@ class _MyHomePageState extends State<MyHomePage> {
           return AlertDialog(
             content: SingleChildScrollView(
                 child: Center(
-              child: Column(
-                children: [
-                  Text(todo != null ? "Detail " : "Tambah " + "Todo"),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    controller: titleTEC,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(), hintText: "Judul"),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextFormField(
-                    controller: detailTEC,
-                    maxLines: 3,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(), hintText: "Detail"),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      ElevatedButton(
+              child: Column(children: [
+                Text((todo != null ? "Detail " : "Tambah ") + " Todo"),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: titleTEC,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: "Judul"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: detailTEC,
+                  maxLines: 4,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: "Detail"),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
                         onPressed: () {
                           Navigator.of(context, rootNavigator: true)
                               .pop('dialog');
                         },
                         child: Text("Batal"),
-                        style: ElevatedButton.styleFrom(primary: Colors.red),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      ElevatedButton(
-                          onPressed: () async {
-                            if (todo == null) {
-                              await addTodo(titleTEC.text, detailTEC.text);
-                            } else {
-                              await updateTodo(
-                                  todo, titleTEC.text, detailTEC.text);
-                            }
-                            setState(() {});
-                            Navigator.of(context, rootNavigator: true)
-                                .pop('dialog');
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.red,
+                        )),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          if (todo != null) {
+                            update(todo, titleTEC.text, detailTEC.text);
+                          } else {
+                            insert(titleTEC.text, detailTEC.text);
+                          }
 
-                            titleTEC.clear();
-                            detailTEC.clear();
-                          },
-                          child: Text("Simpan"))
-                    ],
-                  )
-                ],
-              ),
+                          setState(() {});
+                          Navigator.of(context, rootNavigator: true)
+                              .pop('dialog');
+                          titleTEC.clear();
+                          detailTEC.clear();
+                        },
+                        child: Text("Simpan"))
+                  ],
+                )
+              ]),
             )),
           );
         });
@@ -145,49 +122,52 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text(widget.title),
-      // ),
       backgroundColor: Colors.grey[400],
       body: SafeArea(
-          child: FutureBuilder<List<Todo>>(
-              future: getAllTodo(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState != ConnectionState.waiting) {
-                  return Expanded(
-                    child: ListView.builder(
-                        itemCount: snapshot.data?.length,
-                        itemBuilder: (context, index) {
-                          return Card(
+        child: FutureBuilder<List<Todo>>(
+            future: getAll(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else {
+                print(snapshot.data.toString());
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                      itemCount: snapshot.data?.length,
+                      itemBuilder: (context, index) {
+                        return Card(
                             child: ListTile(
-                              onTap: () {
-                                addOrDetailransaksi(snapshot.data![index]);
-                              },
-                              title: Text(snapshot.data![index].title),
-                              subtitle: Text(snapshot.data![index].detail),
-                              trailing: ElevatedButton(
-                                child: Icon(Icons.delete),
-                                onPressed: () {
-                                  deleteTodo(snapshot.data![index]);
-                                  setState(() {});
+                                onTap: () {
+                                  todoDialog(snapshot.data![index]);
                                 },
-                              ),
-                            ),
-                          );
-                        }),
-                  );
+                                title: Text(snapshot.data![index].title),
+                                subtitle: Text(snapshot.data![index].detail),
+                                trailing: ElevatedButton(
+                                  child: Icon(Icons.delete),
+                                  onPressed: () {
+                                    delete(snapshot.data![index]);
+                                    setState(() {});
+                                  },
+                                )));
+                      });
                 } else {
-                  return Center(child: CircularProgressIndicator());
+                  return Center(
+                    child: Text("Belum ada data"),
+                  );
                 }
-              })),
+              }
+            }),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          // await main();
-          addOrDetailransaksi(null);
+        onPressed: () {
+          titleTEC.clear();
+          detailTEC.clear();
+          todoDialog(null);
         },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
